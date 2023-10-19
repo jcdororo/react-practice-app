d847de7bfda7327880d0e958cd862afe5227b31a
jc <wnwlcks123@gmail.com>
2023-10-19 17:12:18 +0900

    first commit

---
+++ src/components/TodoInput/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const TodoInput = () => {
+  return (
+    <div className='container'>
+      <textarea className='container area'></textarea>
+    </div>
+  )
+}
+
+export default TodoInput
